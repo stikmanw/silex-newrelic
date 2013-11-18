119eea77e157e4eaa79b4f151a6efa354d91b2f4
Jonathan Shieh <jonathan.shieh@gmail.com>
2013-11-18 11:53:27 +0800

    Fixed problem where $app was not passed into configNewRelic. Suppresed undefined index messages when operating on $app['newrelic.options'].

--- src/NewRelic/Silex/NewRelicServiceProvider.php
+++ src/NewRelic/Silex/NewRelicServiceProvider.php
@@ -43,7 +43,7 @@ class NewRelicServiceProvider implements ServiceProviderInterface
             $self->setDefaultOptions($app);
             $self->applyOptions($app);
 
-            return new Newrelic($app['newrelic.options']['exception_if_not_installed']);
+            return new Newrelic(@$app['newrelic.options']['exception_if_not_installed']);
         });
 
         $app['newrelic.ini_configurator'] = $app->share(function($app) {
@@ -106,9 +106,9 @@ class NewRelicServiceProvider implements ServiceProviderInterface
         });
     }
 
-    protected function configureNewRelic()
+    protected function configureNewRelic(Application $app)
     {
-        if ($app['newrelic.options']['disable_auto_rum']) {
+        if (@$app['newrelic.options']['disable_auto_rum']) {
             $app['newrelic']->disableAutoRUM();
         }
 
@@ -123,7 +123,7 @@ class NewRelicServiceProvider implements ServiceProviderInterface
             return;
         }
 
-        $this->configureNewRelic();
+        $this->configureNewRelic($app);
         $this->setupAfterMiddleware($app);
     }
 }
